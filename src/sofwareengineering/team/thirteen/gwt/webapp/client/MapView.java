09594ad343ca1d55db484b4799f7961e11451704
febe19 <fabio.maddaloni@uzh.ch>
2016-11-05 13:45:24 +0100

    First Version of Map View
    GeoChart Markers funktioniert für 10 Beispielstädte. es müsste noch
    einen Richtigen Datenimport gemacht werden.

--- src/sofwareengineering/team/thirteen/gwt/webapp/client/MapView.java
+++ src/sofwareengineering/team/thirteen/gwt/webapp/client/MapView.java
@@ -1,32 +1,27 @@
 package sofwareengineering.team.thirteen.gwt.webapp.client;
 
+import com.google.gwt.core.client.JsArrayString;
 import com.google.gwt.dom.client.Style;
-import com.google.gwt.dom.client.Style.Unit;
 import com.googlecode.gwt.charts.client.*;
 import com.googlecode.gwt.charts.client.geochart.GeoChart;
+import com.googlecode.gwt.charts.client.options.DisplayMode;
 import com.googlecode.gwt.charts.client.geochart.GeoChartColorAxis;
 import com.googlecode.gwt.charts.client.geochart.GeoChartOptions;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.DockLayoutPanel;
-import com.google.gwt.user.client.ui.Label;
-
 
 public class MapView extends Composite {
-	
+
 	private DockLayoutPanel mainPanel = new DockLayoutPanel(Style.Unit.PX);
 	private GeoChart geoChart;
-	
-	//Create the MapView
-	public MapView(){
+
+	// Create the MapView
+	public MapView() {
 		initWidget(mainPanel);
 		initContent();
-	}	
-	
+	}
+
 	private void initContent() {
-		// Shows COntent in MainPanel
-		/*Label l = new Label("Here we will put the Geo Chart of the World map");
-		mainPanel.addNorth(l,20);*/
-		
 		ChartLoader chartLoader = new ChartLoader(ChartPackage.GEOCHART);
 		chartLoader.loadApi(new Runnable() {
 
@@ -41,34 +36,54 @@ public class MapView extends Composite {
 	}
 
 	private void draw() {
-		// Prepare the data
+		// Prepare the data which is shown
+
 		DataTable dataTable = DataTable.create();
-		dataTable.addColumn(ColumnType.STRING, "Country");
-		dataTable.addColumn(ColumnType.NUMBER, "Popularity");
-		dataTable.addRows(6);
-		dataTable.setValue(0, 0, "Germany");
-		dataTable.setValue(0, 1, 200);
-		dataTable.setValue(1, 0, "United States");
-		dataTable.setValue(1, 1, 300);
-		dataTable.setValue(2, 0, "Brazil");
-		dataTable.setValue(2, 1, 400);
-		dataTable.setValue(3, 0, "Canada");
-		dataTable.setValue(3, 1, 500);
-		dataTable.setValue(4, 0, "France");
-		dataTable.setValue(4, 1, 600);
-		dataTable.setValue(5, 0, "RU");
-		dataTable.setValue(5, 1, 700);
+		dataTable.addColumn(ColumnType.STRING, "City");
+		dataTable.addColumn(ColumnType.NUMBER, "Temperature");
+		dataTable.addRows(12);
+		dataTable.setValue(0, 0, "Rome");
+		dataTable.setValue(0, 1, 25);
+		dataTable.setValue(1, 0, "Paris");
+		dataTable.setValue(1, 1, 15);
+		dataTable.setValue(2, 0, "New York");
+		dataTable.setValue(2, 1, 10);
+		dataTable.setValue(3, 0, "Hong Kong");
+		dataTable.setValue(3, 1, 28);
+		dataTable.setValue(4, 0, "Sydney");
+		dataTable.setValue(4, 1, 32);
+		dataTable.setValue(5, 0, "Miami");
+		dataTable.setValue(5, 1, 27);
+		dataTable.setValue(6, 0, "Istanbul");
+		dataTable.setValue(6, 1, 27);
+		dataTable.setValue(7, 0, "Oslo");
+		dataTable.setValue(7, 1, -5);
+		dataTable.setValue(8, 0, "Vancouver");
+		dataTable.setValue(8, 1, -2);
+		dataTable.setValue(9, 0, "Buenos Aires");
+		dataTable.setValue(9, 1, 12);
+		dataTable.setValue(10, 0, "Kapstadt");
+		dataTable.setValue(10, 1, 0);
+		dataTable.setValue(11, 0, "Tokyo");
+		dataTable.setValue(11, 1, 3);
 
-		// Set options
-		GeoChartOptions options = GeoChartOptions.create();
-		GeoChartColorAxis geoChartColorAxis = GeoChartColorAxis.create();
-		/*geoChartColorAxis.setColors("green", "yellow", "red");
-		options.setColorAxis(geoChartColorAxis);*/
-		options.setDatalessRegionColor("gray");
-
-		// Draw the chart
-		geoChart.draw(dataTable, options);
+		geoChart.draw(dataTable, getGeoChartOptions());
 	}
-	
-}
 
+	// Set Style Option for Geocart 											TODO: Size of the markers
+	private GeoChartOptions getGeoChartOptions() {
+		GeoChartOptions options = GeoChartOptions.create();
+		options.setDisplayMode(DisplayMode.MARKERS);
+		GeoChartColorAxis geoChartColorAxis = GeoChartColorAxis.create();
+		geoChartColorAxis.setColors(getNativeArray());
+		options.setColorAxis(geoChartColorAxis);
+		options.setDatalessRegionColor("#373737");
+		return options;
+
+	}
+	// Set Color for Markers
+	private native JsArrayString getNativeArray() /*-{
+		return [ "0000FF", "5858FA", "A9A9F5", "F7819F", "FE2E64", "FF0040" ];
+	}-*/;
+
+}
